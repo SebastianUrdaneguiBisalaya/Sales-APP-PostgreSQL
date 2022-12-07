df8ad52e9759eff9303eea1835a419268f027660
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2022-12-07 09:35:24 -0500

    Improving the UX/UI design of the form.

--- appSale/introduce_information.py
+++ appSale/introduce_information.py
@@ -6,6 +6,7 @@ from tkinter import Frame
 from tkinter import Entry
 from tkinter import Button
 from tkinter import Label
+from tkinter import LabelFrame
 
 class IntroduceInformationInForm:
     def __init__(self):
@@ -23,6 +24,75 @@ class IntroduceInformationInForm:
         label_background_main = tk.Label(self.ventanasec, image=background_main, bg="#fcfcfc")
         label_background_main.place(x=0,y=0,
                                     relheight=1, relwidth=1)
+        
+    # Creating the button
+        Button(self.ventanasec, width=11, pady=4, text="Enviar", bg="#1F3DA7", fg="white",
+               border=1,font=("Inter", 12), relief="flat", cursor="hand2").place(x=505, y=599)
+        
+        Button(self.ventanasec, width=11, pady=4, text="Limpiar", bg="#E8D051", fg="black",
+               border=1,font=("Inter", 12), relief="flat", cursor="hand2").place(x=652, y=599)
+        
+        Button(self.ventanasec, width=11, pady=4, text="Cerrar", bg="#CC4141", fg="white",
+               border=1,font=("Inter", 12), relief="flat", cursor="hand2").place(x=799, y=599)
+        
+    # Creating the main of secondary screen
+        tk.Label(self.ventanasec, text="N° de Orden:", font=("Inter", 11), bg="#FFFFFF").place(x=94,y=209)
+        id_orden = tk.IntVar(0)
+        tk.Entry(self.ventanasec, width=10, fg="black", border=0,
+              bg="#D9D9D9", font=("Inter", 11), textvariable = id_orden,
+              state="readonly", cursor="arrow", justify="center").place(x=190,y=211)
+        
+        self.label_frame_1 = LabelFrame(self.ventanasec, text="Orden", width=820, height=70, font=("Inter", 11), bg="#FFFFFF")
+        self.label_frame_1.place(x=94, y=255)
+        Label(self.label_frame_1, text = "Fecha de orden:", bg="#FFFFFF", font=("Inter",11)).pack(padx=12,pady=12, side="left")
+        Entry(self.label_frame_1, width=13, fg="black", border=0, font=("Inter", 11), bg="#D9D9D9", cursor="arrow",
+              justify="center").pack(padx=12,pady=12, side="left")
+        
+        Label(self.label_frame_1, text = "Fecha de entrega:", bg="#FFFFFF", font=("Inter",11)).pack(padx=12,pady=12, side="left")
+        Entry(self.label_frame_1, width=13, fg="black", border=0, font=("Inter", 11), bg="#D9D9D9", cursor="arrow",
+              justify="center").pack(padx=12,pady=12, side="left")
+        
+        Label(self.label_frame_1, text = "Modo de entrega:", bg="#FFFFFF", font=("Inter",11)).pack(padx=12,pady=12, side="left")
+        Entry(self.label_frame_1, width=13, fg="black", border=0, font=("Inter", 11), bg="#D9D9D9", cursor="arrow",
+              justify="center").pack(padx=14,pady=12, side="left")
+        
+        self.label_frame_2 = LabelFrame(self.ventanasec, text="Cliente", width=820, height=70, font=("Inter", 11), bg="#FFFFFF")
+        self.label_frame_2.place(x=94,y=335)
+        Label(self.label_frame_2, text = "Cliente:", bg="#FFFFFF", font=("Inter",11)).pack(padx=12,pady=12, side="left")
+        Entry(self.label_frame_2, width=30, fg="black", border=0, font=("Inter", 11), bg="#D9D9D9", cursor="arrow",
+              justify="center").pack(padx=22,pady=12, side="left")
+        
+        Label(self.label_frame_2, text = "Segmento del cliente:", bg="#FFFFFF", font=("Inter",11)).pack(padx=12,pady=12, side="left")
+        Entry(self.label_frame_2, width=30, fg="black", border=0, font=("Inter", 11), bg="#D9D9D9", cursor="arrow",
+              justify="center").pack(padx=21,pady=12, side="left")
+        
+        self.label_frame_3 = LabelFrame(self.ventanasec, text="Lugar", width=820, height=70, font=("Inter", 11), bg="#FFFFFF")
+        self.label_frame_3.place(x=94, y=415)
+        
+        Label(self.label_frame_3, text = "País:", bg="#FFFFFF", font=("Inter",11)).pack(padx=12,pady=12, side="left")
+        Entry(self.label_frame_3, width=8, fg="black", border=0, font=("Inter", 11), bg="#D9D9D9", cursor="arrow",
+              justify="center").pack(padx=6,pady=12, side="left")
+        
+        Label(self.label_frame_3, text = "Ciudad:", bg="#FFFFFF", font=("Inter",11)).pack(padx=12,pady=12, side="left")
+        Entry(self.label_frame_3, width=8, fg="black", border=0, font=("Inter", 11), bg="#D9D9D9", cursor="arrow",
+              justify="center").pack(padx=6,pady=12, side="left")
+        
+        Label(self.label_frame_3, text = "Estado:", bg="#FFFFFF", font=("Inter",11)).pack(padx=12,pady=12, side="left")
+        Entry(self.label_frame_3, width=8, fg="black", border=0, font=("Inter", 11), bg="#D9D9D9", cursor="arrow",
+              justify="center").pack(padx=6,pady=12, side="left")
+        
+        Label(self.label_frame_3, text = "Código Postal:", bg="#FFFFFF", font=("Inter",11)).pack(padx=12,pady=12, side="left")
+        Entry(self.label_frame_3, width=8, fg="black", border=0, font=("Inter", 11), bg="#D9D9D9", cursor="arrow",
+              justify="center").pack(padx=6,pady=12, side="left")
+        
+        Label(self.label_frame_3, text = "Region:", bg="#FFFFFF", font=("Inter",11)).pack(padx=12,pady=12, side="left")
+        Entry(self.label_frame_3, width=8, fg="black", border=0, font=("Inter", 11), bg="#D9D9D9", cursor="arrow",
+              justify="center").pack(padx=6,pady=12, side="left")
+        
+        label_frame_4 = LabelFrame(self.ventanasec, text="Producto", width=820, height=70, font=("Inter", 11), bg="#FFFFFF")
+        label_frame_4.place(x=94, y=495)
+        
+        
         
         self.ventanasec.mainloop()
         
