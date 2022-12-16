8d4aaf5f5aa54cfb83b30b2f0a6ccb738b437756
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2022-12-16 13:51:39 -0500

    Creating a dashboard screen

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from appSale.entry_form import ShadowMenu
 from appSale.introduce_information import IntroduceInformationInForm
 from appSale.register_form import RegisterForm
+from appSale.dashboard import Dashboard
 
 if __name__ == "__main__":
     #ShadowMenu()
     #IntroduceInformationInForm()
-    RegisterForm()
+    #RegisterForm()
+    Dashboard()
